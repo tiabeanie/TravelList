a6997d4f00c40f9443d84656e49901d8afbe9727
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-24 12:35:13 -0600

    added destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,7 +1,45 @@
-class DestinationsController < ApplicationController
-
-    get '/destinations' do
-        "Welcome, #{session[:email]}!"
+class DestinationsController < ApplicationController 
+    get "/destinations" do
+      redirect_if_not_logged_in 
+      @destination = Destination.all
+      erb :'destinations/index'
     end
-
-end 
+  
+    get "/destinations/new" do
+      redirect_if_not_logged_in 
+      @error_message = params[:error]
+      erb :'destinations/new'
+    end
+  
+    get "/destinations/:id/edit" do
+      redirect_if_not_logged_in 
+      @error_message = params[:error]
+      @destination = Destination.find(params[:id])
+      erb :'destinations/edit'
+    end
+  
+    post "/destinations/:id" do
+      redirect_if_not_logged_in 
+      @destination = Destination.find(params[:id])
+      unless Destination.valid_params?(params)
+        redirect "/destinations/#{@destination.id}/edit?error=invalid destination"
+      end
+      @destination.update(params.select{|k|k=="name" || k=="country_id"})
+      redirect "/destinations/#{@destination.id}"
+    end
+  
+    get "/destinations/:id" do
+      redirect_if_not_logged_in 
+      @destination = Destination.find(params[:id])
+      erb :'destinations/show'
+    end
+  
+    post "/destinations" do
+      redirect_if_not_logged_in 
+      unless Destination.valid_params?(params)
+        redirect "/destinations/new?error=invalid destination"
+      end
+      Destination.create(params)
+      redirect "/destinations"
+    end
+  end
