4b94f7cac1c606eaf488529fc918659359c5a36c
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-31 12:25:49 -0600

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,62 +1,64 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+  get '/signup' do
+    redirect to '/experiences' if is_logged_in?
 
-    get '/users/:id' do
-      if !logged_in?
-        redirect '/destinations'
-      end
-  
-      @user = User.find(params[:id])
-      if !@user.nil? && @user == current_user
-        erb :'users/show'
-      else
-        redirect '/destinations'
-      end
+    erb :"users/signup"
+  end
+
+  post '/signup' do
+    user_info = { :name => params["name"],
+                  :email => params["email"],
+                  :password => params["password"] }
+
+    is_empty?(user_info, 'signup')
+
+    if User.find_by(:email => user_info[:email])
+      flash[:account_taken] = "The email you provided is already in our system. Please enter a new email or log in to continue."
+      redirect to '/signup'
     end
-  
-    get '/signup' do
-      if !session[:user_id]
-        erb :'users/new'
-      else
-        redirect to '/destinations'
-      end
-    end
-  
-    post '/signup' do 
-      if params[:username] == "" || params[:password] == ""
-        redirect to '/signup'
-      else
-        @user = User.create(:username => params[:username], :password => params[:password])
-        session[:user_id] = @user.id
-        redirect '/destinations'
-      end
-    end
-  
-    get '/login' do 
-      @error_message = params[:error]
-      if !session[:user_id]
-        erb :'users/login'
-      else
-        redirect '/destinations'
-      end
-    end
-  
-    post '/login' do
-      user = User.find_by(:username => params[:username])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "/destinations"
-      else
-        redirect to '/signup'
-      end
-    end
-  
-    get '/logout' do
-      if session[:user_id] != nil
-        session.destroy
+
+    new_user = User.create(user_info)
+    session[:user_id] = new_user.id
+
+    redirect to '/experiences'
+  end
+
+  get '/login' do
+    redirect to '/experiences' if is_logged_in?
+
+    erb :"users/login"
+  end
+
+  post '/login' do
+    user_info = {
+      :email => params["email"],
+      :password => params["password"]
+    }
+
+    is_empty?(user_info, 'login')
+
+    user = User.find_by(:email => user_info[:email])
+
+    if user && user.authenticate(user_info[:password])
+      session[:user_id] = user.id
+      redirect to '/experiences'
+    else
+      if user
+        flash[:password] = "Your password is incorrect"
         redirect to '/login'
       else
-        redirect to '/'
+        flash[:no_account] = "There is no account associated with that email address. Please enter a different email or sign up for an account."
+        redirect to '/login'
       end
     end
-  
   end
+
+  get '/logout' do
+    if is_logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+end
